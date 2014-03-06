d2380c98235fa9d4bfd2c4e245269f59eb014bc1
lazygunner <gymgunner@gmail.com>
2014-03-06 11:23:12 +0800

    add detail view

--- __init__.py
+++ __init__.py
@@ -4,11 +4,12 @@ module = QuokkaModule("mobile", __name__,
                         template_folder="templates")
 
 
-from .views import PostList
+from .views import PostList, PostDetail
 
 
 #module.add_to_template_global()
 
 
 # urls
-module.add_url_rule('/mobile/posts/', view_func=PostList.as_view('posts'))
+module.add_url_rule('/mobile/posts', view_func=PostList.as_view('posts'))
+module.add_url_rule('/mobile/posts/<path:long_slug>/', view_func=PostDetail.as_view('post_detail'))
--- views.py
+++ views.py
@@ -3,6 +3,9 @@ from flask.views import MethodView
 from quokka.core.models import Content
 from datetime import datetime
 
+import time 
+import json
+
 class PostList(MethodView):
     
     def get(self):
@@ -20,9 +23,38 @@ class PostList(MethodView):
 
             post = {
                 'title': content.title,
-                'image': content.get_main_image_url()
+                'image': content.get_main_image_url(),
+                'body': content.get_text()[:250],
+                'c_time': time.mktime(content.created_at.timetuple()),
+                'long_slug' : content.long_slug
             }
+            posts.append(post)
+        return json.dumps(posts)
 
-        
-        return 'hehe'
-        
+class PostDetail(MethodView):
+    
+    def get(self, long_slug):
+            
+        filters = {
+            'published' : True,
+            'available_at__lte' : datetime.now(),
+            'model' : 'posts.post'
+        }
+        try:
+            content = Content.objects.get(
+                long_slug = long_slug,
+                **filters
+            )
+        except Content.DoesNotExist:
+            abort(404)
+ 
+        post = {
+            'title': content.title,
+            'image': content.get_main_image_url(),
+            'body': content.get_text(),
+            'c_time': time.mktime(content.created_at.timetuple()),
+            'long_slug' : content.long_slug
+        }
+
+        return json.dumps(post)
+
